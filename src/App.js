43f979a48e93d9705f31eca6eb08306da221ad53
Ethan <ethan@huegler.net>
2021-11-09 21:29:38 -0500

    setup styles and navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar'
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      test
+      <BrowserRouter>
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
+      <Navbar/>
+      <Routes>
+        
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
