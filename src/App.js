f2806449e14795e177bdad623898bdddbc62dd27
Ethan <ethan@huegler.net>
2021-11-09 22:06:40 -0500

    started fresh

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Navbar from './Components/Navbar/Navbar'
 import './App.scss';
+
+import LandingPage from './Components/LandingPage/LandingPage';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <meta name="viewport" content="width=device-width, initial-scale=1"/>
-      <Navbar/>
-      <Routes>
-        
-      </Routes>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
---
+++ src/Components/LandingPage/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './LandingPage.scss'
+
+function LandingPage() {
+
+  return (
+    <main>
+      <h1>Welcome!</h1>
+    </main>
+  )
+
+}
+
+export default LandingPage
