42d99d00bbcecfde745a7d8fcacb334ac3a99020
Ethan <ethan@huegler.net>
2021-12-26 19:08:44 -0500

    fixed build error

--- pages/_app.js
+++ pages/_app.js
@@ -9,13 +9,13 @@ function MyApp({ Component, pageProps }) {
         <title>Toilet-UMD</title>
         <link rel="icon" href="/favicon.ico" />
 
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"></link>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossOrigin="anonymous"></link>
         
         {/* Font Imports */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Crimson+Pro:wght@500;600;700&display=swap" rel="stylesheet" />
-         
+
       </Head>
 
       <Component {...pageProps} />
