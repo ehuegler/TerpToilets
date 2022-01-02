7302490be5c21b20d098c5afe7f93dff123b0c73
Ethan <ethan@huegler.net>
2022-01-02 14:23:54 -0500

    styles for bathroom summary

--- components/Review.js
+++ components/Review.js
@@ -1,11 +1,35 @@
 import styles from '../styles/components/Review.module.scss'
+import { Rating } from 'react-simple-star-rating'
+import { AiFillStar, AiOutlineStar } from 'react-icons/ai'
+import Image from 'next/image'
 
 export default function Review({ review }) {
-
+    // console.log(review)
+    const date = new Date(review.createdAt)
+    const dateString = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear()
 
     return (
         <div className={styles.review}>
-            {review.message}
+            <div className={styles.author}>{review.author}</div>
+            <div className={styles.date_line}>
+                <Rating
+                    initialValue={review.rating}
+                    readonly
+                    fullIcon={<AiFillStar />}
+                    emptyIcon={<AiOutlineStar />}
+                    className={styles.stars}
+                />
+                &nbsp;
+                {dateString}
+            </div>
+            <div>
+                {review.message}
+            </div>
+            <div>
+                {review.pictures.map(pic => (
+                    
+                ))}
+            </div>
         </div>
     )
 }
