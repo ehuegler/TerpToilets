04df67fe0ddeb0f7ffde71408a2ac5d79b4f27bf
Ethan <ethan@huegler.net>
2022-01-28 15:18:29 -0500

    some form stuff

--- pages/review.js
+++ pages/review.js
@@ -5,16 +5,58 @@ import Main from '../components/Main';
 
 export default function Review() {
 
+  const submit = (event) => {
+    event.preventDefault()
+    console.log(event.target.name.value)
+  }
+
   return (
     <>
       <Head>
 
       </Head>
 
-      <Header />
+      {/* <Header /> */}
 
       <Main>
         Leave a review
+
+        <form onSubmit={submit}
+          className='flex flex-col'
+        >
+
+          <div
+            className='my-2 flex flex-col'
+          >
+            <label htmlFor='name'>Name</label>
+            <input type='text' id='name' autoComplete='name' className='rounded p-1' />
+          </div>
+
+          <div
+            className='my-2 flex flex-col'
+          >
+            <label htmlFor='name'>Message</label>
+            <textarea type='text' id='name' autoComplete='name' className='rounded p-1' />
+          </div>
+
+          <div
+            className='my-2 flex flex-col'
+          >
+            <label htmlFor='name'>Name</label>
+            <input type='text' id='name' autoComplete='name' className='rounded p-1' />
+          </div>
+
+          <button type='submit' 
+            className='flex'
+          >
+            <div
+              className='bg-white drop-shadow-md rounded py-2 px-4'
+            >
+              Submit Review
+            </div>
+          </button>
+
+        </form>
 
       </Main>
     </>
