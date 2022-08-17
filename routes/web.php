4020f2c3be6f6e90f05923f1677a435cf15a3efb
Ethan <ethan@huegler.net>
2022-08-17 01:10:25 -0400

    mixed content issue

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use App\Http\Controllers\BathroomController;
 |
 */
 
+if (App::environment('production')) {
+    URL::forceScheme('https');
+}
+
 Route::get('/', [BathroomController::class, 'index']);
 
 Route::get('/bathrooms/{id}', [BathroomController::class, 'show']);
