78e01f2bdb5f437cc9ef9819480f27af31080341
Ethan <ethan@huegler.net>
2022-08-20 20:59:25 -0400

    gender filtering

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,10 @@
 // deal with all the buildings? here:
 
 let buildingList = document.getElementById('buildingResults');
+let bathroomList = document.getElementById('bathrooms');
 
 toggleBuildingResults = function () {
-    buildingList.classList.toggle('hidden');
+  buildingList.classList.toggle('hidden');
 }
 
 search = function () {
@@ -11,7 +12,7 @@ search = function () {
 
   let input = document.getElementById('buildingInput');
   let query = input.value.toUpperCase();
-  let results = document.getElementById('buildingResults').children;
+  let results = buildingList.children;
 
   for (let result of results) {
     if (result.textContent.toUpperCase().indexOf(query) > -1) {
@@ -21,8 +22,21 @@ search = function () {
     }
   }
 
-} 
+}
 
 buildingClicked = function (building) {
   console.log(building.textContent)
+};
+
+genderFilter = function (select) {
+  let gender = select.value.toUpperCase();
+
+  let results = bathroomList.children;
+  for (let result of results) {
+    if (gender === 'ALL' || result.dataset.gender.toUpperCase() === gender) {
+      result.classList.remove('hidden');
+    } else {
+      result.classList.add('hidden');
+    }
+  }
 }
