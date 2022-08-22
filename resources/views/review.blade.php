74fc2950bd124f14d30fd6729ba7ded9e6aca486
Ethan <ethan@huegler.net>
2022-08-21 23:29:22 -0400

    https form fix

--- resources/views/review.blade.php
+++ resources/views/review.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.app', ['link' => ''])
 
 @section('content')
-    <script>
-        window.onload = function () {
-            buildingClicked(document.getElementById('{{$currBathroom->building->id}}'));
-            document.getElementById('roomList').value = '{{ $currBathroom->id }}';
-        }
-    </script>
+    @if ($currBathroom != '')
+        <script>
+            window.onload = function() {
+                buildingClicked(document.getElementById('{{ $currBathroom->building->id }}'));
+                document.getElementById('roomList').value = '{{ $currBathroom->id }}';
+            }
+        </script>
+    @endif
 
     <div class="bg-white rounded drop-shadow p-4 my-4">
 
@@ -15,9 +17,9 @@
             <label class="font-lg font-crimson">
                 Building
             </label>
-            <input type="text" id="buildingInput" onkeyup="buildingSearch()" onfocus="openBuildingResults(); buildingSearch();"
-                placeholder="&#xf002; Search Buildings" class="w-full bg-bg border border-slate-300 rounded p-2"
-                value="{{ $currBathroom->building->name }}">
+            <input type="text" id="buildingInput" onkeyup="buildingSearch()"
+                onfocus="openBuildingResults(); buildingSearch();" placeholder="&#xf002; Search Buildings"
+                class="w-full bg-bg border border-slate-300 rounded p-2" value="{{ $currBathroom->building->name ?? '' }}">
             <div class="bg-bg rounded p-2 border border-slate-300 my-2 hidden" id="buildingResults">
                 @if (count($buildings) > 0)
                     @foreach ($buildings as $building)
@@ -32,55 +34,43 @@
             </div>
         </div>
 
-        {!! Form::open(['action' => 'BathroomController@store', 'POST']) !!}
+        <form method="POST" action="/store" accept-charset="UTF-8">
 
-        <input type="hidden" id="user" name="user" value="{{ cas()->user() }}">
+            <input type="hidden" id="user" name="user" value="{{ cas()->user() }}">
 
-        <div class="my-4">
-            <label name="id" class="font-lg font-crimson">
-                Bathroom Room Number
-            </label>
-            <select name="id" id="roomList" class="w-full bg-bg border border-slate-300 rounded p-2">
-                <option value="" disabled selected>Make sure a building is selected first...</option>
-            </select>
+            <div class="my-4">
+                <label name="id" class="font-lg font-crimson">
+                    Bathroom Room Number
+                </label>
+                <select name="id" id="roomList" class="w-full bg-bg border border-slate-300 rounded p-2">
+                    <option value="" disabled selected>Make sure a building is selected first...</option>
+                </select>
 
-        </div>
+            </div>
 
-        <div class="my-4">
-            {{ Form::label('author', 'Author:', ['class' => 'font-lg font-crimson']) }}
-            {{ Form::text('author', '', [
-                'class' => 'w-full bg-bg border border-slate-300 rounded p-2',
-                'placeholder' => 'Your Name (optional)',
-            ]) }}
-        </div>
-        <div class="my-4">
-            {{ Form::label('rating', 'Rating:', ['class' => 'font-lg font-crimson']) }}
-            {{ Form::select(
-                'rating',
-                [
-                    '1' => '1',
-                    '2' => '2',
-                    '3' => '3',
-                    '4' => '4',
-                    '5' => '5',
-                ],
-                null,
-                [
-                    'class' => 'w-full bg-bg border border-slate-300 rounded p-2',
-                    'placeholder' => '',
-                ],
-            ) }}
-        </div>
-        <div class="my-4">
-            {{ Form::label('description', 'Description:', ['class' => 'font-lg font-crimson']) }}
-            {{ Form::textarea('description', '', [
-                'class' => 'w-full bg-bg border border-slate-300 rounded p-2',
-                'placeholder' => 'Write your review here...',
-            ]) }}
-        </div>
-        {{ Form::submit('Submit', ['class' => 'bg-umd rounded p-2 text-white']) }}
-        {!! Form::close() !!}
+            <div class="my-4">
+                <label for="author" class="font-lg font-crimson">Author:</label>
+                <input class="w-full bg-bg border border-slate-300 rounded p-2" placeholder="Your Name (optional)"
+                    name="author" type="text" value="" id="author">
+            </div>
+            <div class="my-4">
+                <label for="rating" class="font-lg font-crimson">Rating:</label>
+                <select class="w-full bg-bg border border-slate-300 rounded p-2" id="rating" name="rating">
+                    <option selected="selected" value=""></option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+            </div>
+            <div class="my-4">
+                <label for="description" class="font-lg font-crimson">Description:</label>
+                <textarea class="w-full bg-bg border border-slate-300 rounded p-2" placeholder="Write your review here..."
+                    name="description" cols="50" rows="10" id="description"></textarea>
+            </div>
 
+        </form>
 
     </div>
 @endsection
